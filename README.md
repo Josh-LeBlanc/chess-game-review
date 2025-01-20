c35fb462ff57f73451b7c7f4d046beaa2b501ba6
Josh LeBlanc <joshcapitals07@gmail.com>
2025-01-20 17:00:50 -0500

    organized main function

--- README.md
+++ README.md
@@ -5,11 +5,7 @@ alright right now we just fetch my first game in january and print the first few
 ## stockfish
 download stockfish
 
-__NOT DONE:__
-start it as a process
-
-set options: threads, lines, etc
-
-then eval the position and determine if the next move we make is the same as the best move
+__change of plans:__
+looks like chess package has a better stockfish api than i thought, going to try to use that before making my own
 ## BROKEN:
 it will by default print the white pieces as black and vice versa because the fon't is meant to be a black font. I fixed it by just going into the chess package source code and swapping the characters, but I need to find a permanent fix
--- main.go
+++ main.go
@@ -5,20 +5,32 @@ import (
     "fmt"
     "net/http"
     "os"
+    // "os/exec"
     "io"
     "encoding/json"
     "strings"
 )
 
 func main() {
+    // get pgn of my most recent game
+    last_game_pgn := LastGamePgn()
+
+    // analyze each move with stockfish and point out my bad moves
+    BadMoves(last_game_pgn)
+}
+
+func LastGamePgn() func(*chess.Game) {
     // sample http request with my games from this month
+    // hardcoded this month for now
     r, err := http.Get("https://api.chess.com/pub/player/ggumption/games/2025/01")
     if err != nil {
-        fmt.Println("api request bunked")
-        os.Exit(1)
+        fmt.Errorf("api request error: %w", err);
     }
     defer r.Body.Close()
     body, err := io.ReadAll(r.Body)
+    if err != nil {
+        fmt.Errorf("reading json error: %w", err);
+    }
 
     // process the json
     var d interface{}
@@ -28,19 +40,22 @@ func main() {
     // list of game PGNs
     games := m["games"].([]interface{}) // processes a list
 
-    // first game:
+    // recent game:
     recent_game := games[len(games) - 1].(map[string]interface{}) // string map for fields
     gr := strings.NewReader(recent_game["pgn"].(string)) // convert pgn field to string reader for PGN function
 
     // process the pgn
     pgn, err := chess.PGN(gr)
     if err != nil {
-        fmt.Println("pgn error")
-        os.Exit(1)
+        fmt.Errorf("pgn processing error: %w", err);
     }
 
+    return pgn
+}
+
+func BadMoves(last_game_pgn func(*chess.Game)) {
     // load the pgn into a game and get moves
-    game := chess.NewGame(pgn)
+    game := chess.NewGame(last_game_pgn)
     moves := game.Moves()
 
     // Create new game from the reversed position
@@ -59,7 +74,5 @@ func main() {
             break
         }
     }
-
     
 }
-
